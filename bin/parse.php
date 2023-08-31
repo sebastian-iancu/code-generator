9a89beec8984013901e22e5192a5fa1f57bf98a7
Sebastian Iancu <sebastian.iancu@code24.nl>
2023-08-31 14:26:00 +0200

    adding support for BMM; exporting BMMs for BASE and RM

--- bin/parse.php
+++ bin/parse.php
@@ -4,6 +4,7 @@
 use OpenEHR\Tools\CodeGen\ReadManager;
 use OpenEHR\Tools\CodeGen\WriteManager;
 use OpenEHR\Tools\CodeGen\Writer\InternalModel;
+use OpenEHR\Tools\CodeGen\Writer\BMM;
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
@@ -24,16 +25,24 @@ include $_composer_autoload_path ?? __DIR__ . '/../vendor/autoload.php';
 
 
 $reader = new ReadManager();
+//$reader->read('BASE-v1.1.0.xmi');
 $reader->read('BASE-v1.2.0.xmi');
 $reader->read('RM-v1.1.0.xmi');
 
 $writer = new WriteManager($reader);
-$writer->addWriter(new InternalModel('BASE_and_RM-1.1.0.json'));
+$writer->addWriter(new InternalModel('BASE_and_RM-1.1.0.internal.json'));
+$writer->addWriter(new BMM());
 $writer->write();
 
-$reader->read('AM-v2.2.0.xmi');
+//$reader->read('AM-v2.2.0.xmi');
+//$writer = new WriteManager($reader);
+//$writer->addWriter(new InternalModel('all.json'));
+//$writer->write();
+
+$reader = new ReadManager();
+$reader->read('BASE-v1.1.0.xmi');
 $writer = new WriteManager($reader);
-$writer->addWriter(new InternalModel('all.json'));
+$writer->addWriter(new BMM());
 $writer->write();
 
 
---
+++ src/Writer/BMM.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace OpenEHR\Tools\CodeGen\Writer;
+
+use OpenEHR\Tools\CodeGen\Model\UMLClass;
+use OpenEHR\Tools\CodeGen\Model\UMLEnumeration;
+use OpenEHR\Tools\CodeGen\Model\UMLFile;
+use OpenEHR\Tools\CodeGen\Model\UMLPackage;
+use OpenEHR\Tools\CodeGen\Model\UMLProperty;
+use OpenEHR\Tools\CodeGen\Model\UMLTemplateParameter;
+
+class BMM extends AbstractWriter
+{
+
+    public const REVISION = '1';
+    public const AUTHOR = 'codegen';
+
+    public const SKIP_PACKAGES = ['functional', 'primitive_types', 'builtins'];
+
+    public const PRIMITIVES = [
+        'Any',
+        'Ordered',
+        'Numeric',
+        'Ordered_Numeric',
+        'Byte',
+        'Octet',
+        'Boolean',
+        'Integer',
+        'Integer64',
+        'Real',
+        'Double',
+        'Character',
+        'String',
+        'Uri',
+        'Temporal',
+        'Iso8601_type',
+        'Date',
+        'Time',
+        'Date_time',
+        'Duration',
+        'Iso8601_date',
+        'Iso8601_time',
+        'Iso8601_date_time',
+        'Iso8601_duration',
+        'Terminology_term',
+        'Terminology_code',
+        'Container',
+        'List',
+        'Array',
+        'Set',
+        'Interval',
+        'Cardinality',
+        'Multiplicity_interval',
+        'Hash',
+    ];
+
+    public function __construct()
+    {
+    }
+
+    public function setDir(string $dir): void
+    {
+        $dir .= DIRECTORY_SEPARATOR . 'InternalModel';
+        parent::setDir($dir);
+    }
+
+
+    public function write(): void
+    {
+        /** @var UMLFile $umlFile */
+        /** @var UMLPackage $umlPackage */
+        /** @var UMLPackage $childUmlPackage */
+        /** @var UMLClass $umlClass */
+        foreach ($this->reader->umlFiles as $umlFile) {
+            $schema_name = strtolower($umlFile->name);
+            $schema = [
+                'bmm_version' => '2.3',
+                'rm_publisher' => 'openehr',
+                'schema_name' => $schema_name,
+                'rm_release' => $umlFile->getRelease(),
+                'schema_revision' => $umlFile->getRelease() . '.' . self::REVISION,
+                'schema_lifecycle_state' => 'stable',
+                'schema_description' => $umlFile->umlPackage->description,
+                'schema_author' => self::AUTHOR,
+            ];
+            $collectedUmlClasses = [];
+            // serializing packages and their classes
+            foreach ($umlFile->umlPackage->getPackages('org::openehr::' . $schema_name . '::*') as $umlPackage) {
+                if (in_array($umlPackage->name, self::SKIP_PACKAGES)) {
+                    continue;
+                }
+                $bmmPackage = self::asBmmPackage($umlPackage, "org.openehr.{$schema_name}.", $collectedUmlClasses);
+                $schema['packages'][$bmmPackage['name']] = $bmmPackage;
+            }
+            // serializing primitives and class
+            $schema['primitives'] = [];
+            $schema['class_definitions'] = [];
+            foreach ($collectedUmlClasses as $umlClass) {
+                if (in_array($umlClass->name, self::PRIMITIVES)) {
+                    $schema['primitives'][$umlClass->name] = self::asBmmClass($umlClass);
+                } else {
+                    $schema['class_definitions'][$umlClass->name] = self::asBmmClass($umlClass);
+                }
+            }
+            // saving as file
+            $filename = $this->dir . DIRECTORY_SEPARATOR . str_replace('.xmi', '', $umlFile->id) . '.bmm.json';
+            $bytes = file_put_contents($filename, json_encode($schema, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT));
+            $this->log('  Wrote %s bytes to %s file.', $bytes, $filename);
+        }
+    }
+
+    protected static function asBmmPackage(UMLPackage $umlPackage, string $namePrefix = '', &$collectedUmlClasses = []): array
+    {
+        $bmmPackage = [
+            'name' => $namePrefix . $umlPackage->name,
+            'packages' => [],
+            'classes' => self::collectClassNames($umlPackage, $collectedUmlClasses),
+        ];
+        foreach ($umlPackage->umlPackages as $childUmlPackage) {
+            if (in_array($childUmlPackage->name, self::SKIP_PACKAGES)) {
+                continue;
+            }
+            $bmmChildUmlPackage = self::asBmmPackage($childUmlPackage, '', $collectedUmlClasses);
+            $bmmPackage['packages'][$bmmChildUmlPackage['name']] = $bmmChildUmlPackage;
+        }
+        return array_filter($bmmPackage);
+    }
+
+    protected static function collectClassNames(UMLPackage $umlPackage, &$collectedUmlClasses = []): array
+    {
+        $names = [];
+        foreach ($umlPackage->umlClasses as $umlClass) {
+            if (str_contains($umlClass->name, '<')) {
+                continue;
+            }
+            $names[] = $umlClass->name;
+            $collectedUmlClasses[] = $umlClass;
+        }
+        return $names;
+    }
+
+    protected static function asBmmClass(UMLClass|UMLEnumeration $umlClass): array
+    {
+        $bmmClass = [
+            'name' => $umlClass->name,
+        ];
+        if ($umlClass instanceof UMLClass) {
+            if ($umlClass->isAbstract) {
+                $bmmClass['is_abstract'] = true;
+            }
+            if ($umlClass->umlGeneralizations->count()) {
+                $bmmClass['ancestors'] = array_keys((array)$umlClass->umlGeneralizations);
+            }
+            $bmmClass['documentation'] = $umlClass->description;
+            foreach ($umlClass->umlTemplateParameters as $umlTemplateParameter) {
+                $bmmClass['generic_parameter_defs'][$umlTemplateParameter->name] = self::asBmmGenericParameterDefs($umlTemplateParameter);
+            }
+            foreach ($umlClass->umlProperties as $umlProperty) {
+                $bmmClass['properties'][$umlProperty->name] = self::asBmmProperty($umlProperty, $umlClass);
+            }
+        }
+        if ($umlClass instanceof UMLEnumeration) {
+            $bmmClass['p_bmm_type'] = 'P_BMM_ENUMERATION_STRING';
+            $bmmClass['ancestors'] = ['String'];
+            $bmmClass['documentation'] = $umlClass->description;
+            $bmmClass['item_names'] = $umlClass->enumerations;
+        }
+
+        return $bmmClass;
+    }
+
+    protected static function asBmmProperty(UMLProperty $umlProperty, UMLClass $umlClass): array
+    {
+        $bmmProperty = [
+            'name' => $umlProperty->name,
+        ];
+        if ($umlProperty->templateParameterId) {
+            $bmmProperty['p_bmm_type'] = 'P_BMM_SINGLE_PROPERTY_OPEN';
+            $bmmProperty['type'] = $umlClass->umlTemplateParameters->get($umlProperty->templateParameterId)->name;
+        } elseif (str_contains($umlProperty->type->name, '<')) {
+            $bmmProperty['p_bmm_type'] = 'P_BMM_GENERIC_PROPERTY';
+//            $bmmProperty['type_def'] = $umlProperty->type->name;
+            $bmmProperty['type_def'] = self::asTypeDef($umlProperty->type->name);
+        } elseif ($umlProperty->maxOccurs === -1) {
+            $bmmProperty['p_bmm_type'] = 'P_BMM_CONTAINER_PROPERTY';
+            $bmmProperty['type_def'] = [
+                'container_type' => 'List',
+                'type' => $umlProperty->type->name,
+            ];
+            $bmmProperty['cardinality'] = "|>={$umlProperty->minOccurs}|";
+        } else {
+            $bmmProperty['p_bmm_type'] = 'P_BMM_SINGLE_PROPERTY';
+            $bmmProperty['type'] = $umlProperty->type->name;
+        }
+        if ($umlProperty->minOccurs) {
+            $bmmProperty['is_mandatory'] = true;
+        }
+        $bmmProperty['documentation'] = $umlProperty->description;
+        return $bmmProperty;
+    }
+
+    public static function asTypeDef(string $descriptor): array
+    {
+        $typeDef = [];
+        if (preg_match('/^(\w+)\<(.*)\>$/', $descriptor, $m)) {
+            $typeDef['root_type'] = $m[1];
+            if (str_contains($m[2], '<')) {
+                $descriptorPart = $m[2];
+                $typeDef['generic_parameter_defs'] = [];
+                while (preg_match('/^(\w+)(\<(?:([^\<\>]*)|(?:(?3)(?2)(?3))*)\>)?(?:,\s*)?/', $descriptorPart, $p)) {
+                    $descriptorPart = substr($descriptorPart, strlen($p[0]));
+                    if (empty($p[2])) {
+                        $typeDef['generic_parameter_defs'][] = [
+                            'p_bmm_type' => 'P_BMM_SIMPLE_TYPE',
+                            'type' => $p[1],
+                        ];
+                    } else {
+                        $typeDef['generic_parameter_defs'][] = array_merge([
+                            'p_bmm_type' => 'P_BMM_GENERIC_TYPE',
+                        ], self::asTypeDef($p[1].$p[2]));
+                    }
+                }
+            } else {
+                $typeDef['generic_parameters'] = explode(', ', $m[2]);
+            }
+        } else {
+            $typeDef['err_type_def'] = $descriptor;
+        }
+        return $typeDef;
+    }
+
+    protected static function asBmmGenericParameterDefs(UMLTemplateParameter $UMLTemplateParameter): array
+    {
+        $bmmGenericParameterDef = [
+            'name' => $UMLTemplateParameter->name,
+        ];
+        if ($UMLTemplateParameter->type->referenceMethod !== 'implicit') {
+            $bmmGenericParameterDef['conforms_to_type'] = $UMLTemplateParameter->type->name;
+        }
+        return $bmmGenericParameterDef;
+    }
+}
